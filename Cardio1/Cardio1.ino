77b142089c70286b93a6daa4c65936de8d100038
Ryan McMahon <ryanm35@cs.washington.edu>
2014-12-03 21:06:26 -0800

    menu added

--- Cardio1/Cardio1.ino
+++ Cardio1/Cardio1.ino
@@ -11,6 +11,14 @@ PDB triggers the ADC which requests the DMA to move the data to a buffer
 #define PDB_CH0C1_TOS 0x0100
 #define PDB_CH0C1_EN 0x01
 
+#define GRAPH_BACKGROUND ILI9341_WHITE
+#define GRAPH_GRID_LINES ILI9341_GREEN
+#define GRAPH_LINE ILI9341_BLACK
+#define MENU_BACKGROUND ILI9341_BLACK
+#define CALIBRATING ILI9341_BLACK
+#define MENU_HEADER ILI9341_RED
+#define MENU_TEXT ILI9341_WHITE
+
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 
 volatile uint32_t PDB_CONFIG;
@@ -29,10 +37,11 @@ uint16_t lineDelta;
 uint16_t xPos = 0;
 volatile uint16_t queueSize = 0;
 uint16_t blankSpace = 0;
-volatile boolean on = false;
+volatile boolean reading_state = false;
 int prev = HIGH;
 uint32_t count = 0;
-
+boolean menu_state = true;
+boolean to_menu_state = false;
 
 volatile boolean hasData;
 volatile uint32_t adcData;
@@ -75,34 +84,97 @@ void addQueue(uint32_t val) {
   
 void setup() {
   tft.begin();
-  tft.setRotation(3);
+  tft.setRotation(1);
   pinMode(1, INPUT_PULLUP);
-  tft.fillScreen(ILI9341_WHITE);
+  
   Serial.begin(9600);
   //while (!Serial);
-  grid_delta = tft.width() / num_vert_lines;
-  Serial.println(tft.width());
-  Serial.println(num_vert_lines);
-  Serial.println(grid_delta);
-  initVertLines();
-  initHorLines();
-  pinMode(HEART_INPUT, INPUT);
-  lineDelta = tft.width() / (QUEUE_LENGTH - 1);
   
+  grid_delta = tft.width() / num_vert_lines;
+  lineDelta = tft.width() / (QUEUE_LENGTH - 1);
 
+  pinMode(HEART_INPUT, INPUT);
+  
+  
   adcInit();
   pdbInit();
   
-  addQueue(0);
- 
-  
   NVIC_ENABLE_IRQ(IRQ_PDB);
+  initMenuState();
+  
 }
 
-uint16_t grid_color = ILI9341_GREEN;
+
+void initReadingDataState(uint32_t time) {
+    tft.fillScreen(GRAPH_BACKGROUND);
+    tft.setCursor(tft.width() / 2 - 80, tft.height() / 2);
+    tft.setTextColor(CALIBRATING);
+    tft.setTextSize(2);
+    tft.print("Callibrating....");
+    int maxV = -1;
+    int minV = 1024;
+    int index = 0;
+    int calBuf[200];
+    int calBufSize = 0;
+    while(abs(maxV - minV) > 800) {
+      if(!hasData) {
+        continue;
+      }
+      calBuf[calBufSize % 200] = adcData;
+      calBufSize++;
+      hasData = false;
+      if(calBufSize < 200) {
+        continue;
+      }
+      maxV = -1;
+      minV = 1024;
+      for(int i  = 0; i < 200; i++) {
+        maxV = max(calBuf[i], maxV);
+        minV = min(calBuf[i], minV);
+      }
+    }
+    
+    
+    //init the graph
+    tft.fillScreen(GRAPH_BACKGROUND);
+    initVertLines();
+    initHorLines();
+    
+    //reset the queue
+    startPoint = 0;
+    endPoint = 0;
+    queueSize = 0;
+    xPos = 0;
+    
+    //reset the sd and time
+    count = time;
+    sdIndex = 0;
+    
+    //add the first filler data point
+    addQueue(0);
+    
+    //start reading
+    menu_state = false;
+    reading_state = true;
+}
+
+void initMenuState() {
+  reading_state = false;
+  tft.fillScreen(MENU_BACKGROUND);
+  tft.setTextColor(MENU_HEADER);
+  tft.setTextSize(3);
+  tft.setCursor(45, 10); tft.print("EKG Monitor");
+  tft.setTextSize(2);
+  tft.setTextColor(MENU_TEXT);
+  tft.setCursor(30, 80); tft.print("Begin reading");
+  menu_state = true;
+  to_menu_state = false;
+  
+}
+
 void initVertLines() {
   for(int i = 0; i <= num_vert_lines; i++) {
-    tft.drawLine(i * grid_delta, 0, i * grid_delta, tft.height(), grid_color);
+    tft.drawLine(i * grid_delta, 0, i * grid_delta, tft.height(), GRAPH_GRID_LINES);
   }
 }
 
@@ -113,14 +185,14 @@ void redrawHorLines(int x1, int y1, int x2, int y2) {
   int finishingLine = maxY / grid_delta + 1;
   for(int i = startingLine ; i <= finishingLine; i++) {
     if(i * grid_delta <= maxY && i * grid_delta >= minY) {
-      tft.drawLine(x1, i * grid_delta, x2, i * grid_delta,  grid_color);
+      tft.drawLine(x1, i * grid_delta, x2, i * grid_delta,  GRAPH_GRID_LINES);
     }
   }
 }
 
 void initHorLines() {
   for(int i = 0; i <= tft.height() / grid_delta; i++) {
-    tft.drawLine(0, i * grid_delta, tft.width(), i * grid_delta, grid_color);
+    tft.drawLine(0, i * grid_delta, tft.width(), i * grid_delta, GRAPH_GRID_LINES);
   }
 }
 
@@ -131,7 +203,7 @@ void redrawVertLines(int x1, int y1, int x2, int y2) {
   int finishingLine = x2 / grid_delta + 1;
   for(int i = startingLine; i <= finishingLine; i++) {
     if(i * grid_delta <= x2 && i * grid_delta >= x1) {
-      tft.drawLine(i * grid_delta, minY - 1, i * grid_delta, maxY + 1, grid_color);
+      tft.drawLine(i * grid_delta, minY - 1, i * grid_delta, maxY + 1, GRAPH_GRID_LINES);
     }
   }
 }
@@ -145,36 +217,29 @@ void writeToSD() {
 int i2 = 0;
 
 uint32_t startTime;
-volatile boolean stopButton;
+volatile boolean to_munu_state;
 
 void loop() {
   uint32_t time = millis();
   int sensorVal = digitalRead(1);
-
+  
   if (prev != sensorVal && sensorVal == LOW) {
-    on = !on;
-    if(on) {
-      count = time;
-      sdIndex = 0;
-      stopButton = false;
-    } else {
-      stopButton = true;
+    if(menu_state) {
+      initReadingDataState(time);
+    } else if(reading_state) {
+      to_menu_state = true;    
     }
+  }
+  prev = sensorVal;
     
-   }
-   prev = sensorVal;
-    
-   if(stopButton || on && time - count > 300000 || sdIndex == HERTZ * RECORD_TIME) {
-     on = false;
-     stopButton  = false;
-     writeToSD(); 
-     sdIndex = 0;
-   } 
+ if(to_menu_state || reading_state && time - count > 300000 || sdIndex == HERTZ * RECORD_TIME) {
+   writeToSD(); 
+   initMenuState();
+ } 
    
-  if (hasData && on) {
+  if (hasData && reading_state) {
       hasData = false;    
-      uint32_t yVal = tft.height() * adcData / 1023;
-      //uint32_t yVal = tft.height() * (ADC0_RA / 1023) /  (((float) tft.height() / grid_delta) / 3.3);
+      uint32_t yVal = tft.height() * adcData / 4095;
       if (queueSize == QUEUE_LENGTH) {
         uint32_t oldVal = removeQueue();
         uint32_t oldVal2 = peekQueue();
@@ -183,18 +248,18 @@ void loop() {
           xStart = 0;
         }
         tft.drawLine(xStart, oldVal, xStart + lineDelta,
-                      oldVal2, ILI9341_WHITE);
+                      oldVal2, GRAPH_BACKGROUND);
         tft.drawLine(xStart, oldVal + 1, xStart + lineDelta,
-                      oldVal2 + 1, ILI9341_WHITE);                
+                      oldVal2 + 1, GRAPH_BACKGROUND);                
         tft.drawLine(xStart, oldVal - 1, xStart + lineDelta,
-                      oldVal2 - 1, ILI9341_WHITE);
+                      oldVal2 - 1, GRAPH_BACKGROUND);
         redrawVertLines(xStart, oldVal, xStart + lineDelta, oldVal2);
         redrawHorLines(xStart, oldVal, xStart + lineDelta, oldVal2);
       } 
       uint32_t temp = peekEndQueue();
-      tft.drawLine(xPos, temp, xPos + lineDelta, yVal, ILI9341_BLACK);
-      tft.drawLine(xPos, temp - 1, xPos + lineDelta, yVal - 1, ILI9341_BLACK);
-      tft.drawLine(xPos, temp + 1, xPos + lineDelta, yVal + 1, ILI9341_BLACK);
+      tft.drawLine(xPos, temp, xPos + lineDelta, yVal, GRAPH_LINE);
+      tft.drawLine(xPos, temp - 1, xPos + lineDelta, yVal - 1, GRAPH_LINE);
+      tft.drawLine(xPos, temp + 1, xPos + lineDelta, yVal + 1, GRAPH_LINE);
       addQueue(yVal);
       xPos += lineDelta;
       if (xPos > tft.width()) {
@@ -215,7 +280,7 @@ static const uint8_t channel2sc1a[] = {
 	ADC_CFG1_MODE(2)         Single ended 10 bit mode
 	ADC_CFG1_ADLSMP          Long sample time
 */
-#define ADC_CONFIG1 (ADC_CFG1_ADIV(1) | ADC_CFG1_MODE(2) | ADC_CFG1_ADLSMP)
+#define ADC_CONFIG1 (ADC_CFG1_ADIV(1) | ADC_CFG1_MODE(1) | ADC_CFG1_ADLSMP)
 
 /*
 	ADC_CFG2_MUXSEL          Select channels ADxxb
@@ -293,11 +358,9 @@ void pdbInit() {
 }
 
 
-
 void adc0_isr() {
     adcData = ADC0_RA;
-    hasData = true;
-      
+    hasData = true;   
 }
 
 void pdb_isr() {
